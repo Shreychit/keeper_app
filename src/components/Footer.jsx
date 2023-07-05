9df89453a5821e57a91cfc1b591d6c08e3adf6f5
Shreyas Chitransh <shreyaschit15@gmail.com>
2023-07-06 01:52:34 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <footer>
+            <p>Made by Shreyas Chitransh</p>
+        </footer>
+    )
+}
+
+export default Footer;
